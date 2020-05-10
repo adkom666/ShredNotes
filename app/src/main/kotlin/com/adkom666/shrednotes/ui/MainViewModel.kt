b9f16d3bc4435a13c1b978f44457b21853736efd
kadomtsev <kadomtsev@softinvent.ru>
2020-05-10 21:18:50 +0300

    Main screen model docs

--- app/src/main/kotlin/com/adkom666/shrednotes/ui/MainViewModel.kt
+++ app/src/main/kotlin/com/adkom666/shrednotes/ui/MainViewModel.kt
@@ -31,36 +31,60 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
     private val _sectionAsLiveData: MutableLiveData<Section> =
         MutableLiveData(DEFAULT_SECTION)
 
+    /**
+     * Subscribe for search visibility on UI thread.
+     */
     val isSearchVisibleAsLiveData: LiveData<Boolean> =
         distinctUntilChanged(_isSearchVisibleAsLiveData)
 
+    /**
+     * Subscribe for filter visibility on UI thread.
+     */
     val isFilterVisibleAsLiveData: LiveData<Boolean> =
         distinctUntilChanged(_isFilterVisibleAsLiveData)
 
+    /**
+     * Subscribe for filter enablement on UI thread.
+     */
     val isFilterEnabledAsLiveData: LiveData<Boolean> =
         distinctUntilChanged(_isFilterEnabledAsLiveData)
 
+    /**
+     * Subscribe for current section on UI thread.
+     */
     val sectionAsLiveData: LiveData<Section> =
         distinctUntilChanged(_sectionAsLiveData)
 
+    /**
+     * Get and set search visibility on UI thread.
+     */
     var isSearchVisible: Boolean
         get() = _isSearchVisibleAsLiveData.value ?: IS_SEARCH_VISIBLE_BY_DEFAULT
         set(value) {
             _isSearchVisibleAsLiveData.value = value
         }
 
+    /**
+     * Get and set filter visibility on UI thread.
+     */
     var isFilterVisible: Boolean
         get() = _isFilterVisibleAsLiveData.value ?: IS_FILTER_VISIBLE_BY_DEFAULT
         set(value) {
             _isFilterVisibleAsLiveData.value = value
         }
 
+    /**
+     * Get and set filter enablement on UI thread.
+     */
     var isFilterEnabled: Boolean
         get() = _isFilterEnabledAsLiveData.value ?: IS_FILTER_ENABLED_BY_DEFAULT
         set(value) {
             _isFilterEnabledAsLiveData.value = value
         }
 
+    /**
+     * Get and set current section on UI thread.
+     */
     var section: Section
         get() = _sectionAsLiveData.value ?: DEFAULT_SECTION
         set(value) {
