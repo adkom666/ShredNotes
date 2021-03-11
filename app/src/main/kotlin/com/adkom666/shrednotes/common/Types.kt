4863d7092ea1127ca4c189dde56f3d64a72c74c8
adkom666 <adkom666@gmail.com>
2021-03-11 17:44:13 +0300

    Ensure ID type compatibility

--- app/src/main/kotlin/com/adkom666/shrednotes/common/Types.kt
+++ app/src/main/kotlin/com/adkom666/shrednotes/common/Types.kt
@@ -4,3 +4,13 @@ package com.adkom666.shrednotes.common
  * Identifier of the collection item.
  */
 typealias Id = Long
+
+/**
+ * Ensure type compatibility with Long.
+ */
+fun Long.toId() = this
+
+/**
+ * Ensure type compatibility with Int.
+ */
+fun Int.toId() = this.toLong()
