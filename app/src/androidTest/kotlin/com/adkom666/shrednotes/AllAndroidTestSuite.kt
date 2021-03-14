1080ee31dea399e2aff3a5395f1004c48066bf84
adkom666 <adkom666@gmail.com>
2021-03-14 06:52:42 +0300

    Store note test in all Android test list

--- app/src/androidTest/kotlin/com/adkom666/shrednotes/AllAndroidTestSuite.kt
+++ app/src/androidTest/kotlin/com/adkom666/shrednotes/AllAndroidTestSuite.kt
@@ -1,11 +1,13 @@
 package com.adkom666.shrednotes
 
 import com.adkom666.shrednotes.data.db.StoreExerciseTest
+import com.adkom666.shrednotes.data.db.StoreNoteTest
 import org.junit.runner.RunWith
 import org.junit.runners.Suite
 
 @RunWith(Suite::class)
 @Suite.SuiteClasses(
-    StoreExerciseTest::class
+    StoreExerciseTest::class,
+    StoreNoteTest::class
 )
 class AllAndroidTestSuite
