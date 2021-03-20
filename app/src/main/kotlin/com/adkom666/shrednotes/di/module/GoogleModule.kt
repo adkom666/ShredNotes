27dd3d3f0f307cc7c770b2100b9ef2d16e2ab621
adkom666 <adkom666@gmail.com>
2021-03-20 14:00:15 +0300

    Google is singleton

--- app/src/main/kotlin/com/adkom666/shrednotes/di/module/GoogleModule.kt
+++ app/src/main/kotlin/com/adkom666/shrednotes/di/module/GoogleModule.kt
@@ -4,12 +4,14 @@ import android.content.Context
 import com.adkom666.shrednotes.data.google.Google
 import dagger.Module
 import dagger.Provides
+import javax.inject.Singleton
 
 @Suppress("UndocumentedPublicClass", "UndocumentedPublicFunction")
 @Module(includes = [ContextModule::class])
 class GoogleModule {
 
     @Provides
+    @Singleton
     fun google(context: Context): Google {
         return Google(context)
     }
