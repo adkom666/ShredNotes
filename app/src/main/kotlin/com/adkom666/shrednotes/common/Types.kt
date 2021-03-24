6e870b6c866dd8bebff67314adce98e2e8be33fd
adkom666 <adkom666@gmail.com>
2021-03-24 21:02:26 +0300

    Minor edits

--- app/src/main/kotlin/com/adkom666/shrednotes/common/Types.kt
+++ app/src/main/kotlin/com/adkom666/shrednotes/common/Types.kt
@@ -1,9 +1,13 @@
+@file:Suppress("unused")
+
 package com.adkom666.shrednotes.common
 
 /**
  * Identifier of the collection item.
  */
 typealias Id = Long
+
+const val NO_ID = 0L
 
 /**
  * Ensure type compatibility with Long.
@@ -14,5 +18,3 @@ fun Long.toId() = this
  * Ensure type compatibility with Int.
  */
 fun Int.toId() = this.toLong()
-
-const val NO_ID = 0L
