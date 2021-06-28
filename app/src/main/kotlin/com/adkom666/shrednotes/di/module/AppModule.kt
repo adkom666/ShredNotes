ccf185356392336abd1d4957db384d5572c5293b
adkom666 <adkom666@gmail.com>
2021-06-28 08:09:32 +0300

    Register statistics & ask fragments in DI

--- app/src/main/kotlin/com/adkom666/shrednotes/di/module/AppModule.kt
+++ app/src/main/kotlin/com/adkom666/shrednotes/di/module/AppModule.kt
@@ -1,10 +1,12 @@
 package com.adkom666.shrednotes.di.module
 
 import com.adkom666.shrednotes.ui.MainActivity
+import com.adkom666.shrednotes.ui.ask.AskFragment
 import com.adkom666.shrednotes.ui.exercises.ExerciseActivity
 import com.adkom666.shrednotes.ui.exercises.ExercisesFragment
 import com.adkom666.shrednotes.ui.notes.NoteActivity
 import com.adkom666.shrednotes.ui.notes.NotesFragment
+import com.adkom666.shrednotes.ui.statistics.StatisticsFragment
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -28,4 +30,10 @@ abstract class AppModule {
 
     @ContributesAndroidInjector
     abstract fun noteActivity(): NoteActivity
+
+    @ContributesAndroidInjector
+    abstract fun statisticsFragment(): StatisticsFragment
+
+    @ContributesAndroidInjector
+    abstract fun askFragment(): AskFragment
 }
