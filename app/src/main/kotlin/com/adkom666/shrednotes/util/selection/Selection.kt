b5c838057182c91ba5051b941011a56d346ec59c
adkom666 <adkom666@gmail.com>
2021-06-29 09:04:35 +0300

    Minor documentation edit

--- app/src/main/kotlin/com/adkom666/shrednotes/util/selection/Selection.kt
+++ app/src/main/kotlin/com/adkom666/shrednotes/util/selection/Selection.kt
@@ -11,14 +11,16 @@ interface Selection {
     val isActive: Boolean
 
     /**
-     * Addition of the listener which detects when at least one selected item appears or disappears.
+     * Addition of the listener which detects when at least one selected item appears or no more
+     * selected items.
      *
      * @param listener listener to add.
      */
     fun addOnActivenessChangeListener(listener: OnActivenessChangeListener)
 
     /**
-     * Removing of the listener which detects when at least one selected item appears or disappears.
+     * Removing of the listener which detects when at least one selected item appears or no more
+     * selected items.
      *
      * @param listener listener to remove.
      */
