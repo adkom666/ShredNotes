c063aa54c553cb7b21fa2764eac06bf1c3c5b7ac
adkom666 <adkom666@gmail.com>
2021-09-01 15:03:12 +0300

    Predefined test statistics by day of the week

--- app/src/androidTest/kotlin/com/adkom666/shrednotes/statistics/WeekdaysStatisticsAggregatorTest.kt
+++ app/src/androidTest/kotlin/com/adkom666/shrednotes/statistics/WeekdaysStatisticsAggregatorTest.kt
@@ -1,15 +1,13 @@
 package com.adkom666.shrednotes.statistics
 
+import com.adkom666.shrednotes.common.toId
 import com.adkom666.shrednotes.data.db.ShredNotesDatabase
-import com.adkom666.shrednotes.data.db.StoreExerciseTestHelper
-import com.adkom666.shrednotes.data.db.StoreNoteTestHelper
 import com.adkom666.shrednotes.data.db.dao.ExerciseDao
 import com.adkom666.shrednotes.data.db.dao.NoteDao
+import com.adkom666.shrednotes.data.db.entity.ExerciseEntity
 import com.adkom666.shrednotes.data.db.entity.NoteEntity
 import com.adkom666.shrednotes.di.component.DaggerTestStatisticsComponent
-import com.adkom666.shrednotes.util.time.Days
 import java.util.Calendar
-import java.util.Date
 import javax.inject.Inject
 import junit.framework.TestCase
 import kotlin.time.ExperimentalTime
@@ -20,7 +18,9 @@ import timber.log.Timber
 class WeekdaysStatisticsAggregatorTest : TestCase() {
 
     private companion object {
-        private val ALL_WEEKDAYS = Weekday.values()
+        private const val EXERCISE_NAME_1 = "exercise 1"
+        private const val EXERCISE_NAME_2 = "exercise 2"
+        private val BPM = arrayOf(256, 666, 512, 300)
     }
 
     @Inject
@@ -38,8 +38,7 @@ class WeekdaysStatisticsAggregatorTest : TestCase() {
     override fun setUp() {
         super.setUp()
         DaggerTestStatisticsComponent.create().inject(this)
-        StoreExerciseTestHelper.insertExercises(exerciseDao)
-        StoreNoteTestHelper.insertNotes(noteDao, exerciseDao)
+        fillDatabase()
     }
 
     override fun tearDown() {
@@ -48,86 +47,80 @@ class WeekdaysStatisticsAggregatorTest : TestCase() {
     }
 
     fun testAggregateAverageAmongMaxBpm() = runBlocking {
-        val averageAmongMaxBpm = aggregateAverageQuantity { noteList ->
-            noteList.map { it.bpm }.maxOrNull()
-        }
-
         val statistics = statisticsAggregator.aggregateAverageAmongMaxBpm()
         Timber.d("statistics.valueMap=${statistics.valueMap}")
-
-        ALL_WEEKDAYS.forEach { weekday ->
-            assertEquals(statistics.valueMap[weekday], averageAmongMaxBpm[weekday])
-        }
+        assertEquals(null, statistics.valueMap[Weekday.SUNDAY])
+        assertEquals(BPM[0].toFloat(), statistics.valueMap[Weekday.MONDAY])
+        assertEquals(null, statistics.valueMap[Weekday.TUESDAY])
+        assertEquals((BPM[1] + BPM[3]).toFloat() / 2, statistics.valueMap[Weekday.WEDNESDAY])
+        assertEquals(null, statistics.valueMap[Weekday.THURSDAY])
+        assertEquals(null, statistics.valueMap[Weekday.FRIDAY])
+        assertEquals(null, statistics.valueMap[Weekday.SATURDAY])
     }
 
     fun testAggregateAverageNoteCount() = runBlocking {
-        val averageNoteCount = aggregateAverageQuantity { noteList ->
-            noteList.count()
-        }
-
         val statistics = statisticsAggregator.aggregateAverageNoteCount()
         Timber.d("statistics.valueMap=${statistics.valueMap}")
-
-        ALL_WEEKDAYS.forEach { weekday ->
-            assertEquals(statistics.valueMap[weekday], averageNoteCount[weekday])
-        }
+        assertEquals(null, statistics.valueMap[Weekday.SUNDAY])
+        assertEquals(1f, statistics.valueMap[Weekday.MONDAY])
+        assertEquals(null, statistics.valueMap[Weekday.TUESDAY])
+        assertEquals(1.5f, statistics.valueMap[Weekday.WEDNESDAY])
+        assertEquals(null, statistics.valueMap[Weekday.THURSDAY])
+        assertEquals(null, statistics.valueMap[Weekday.FRIDAY])
+        assertEquals(null, statistics.valueMap[Weekday.SATURDAY])
     }
 
-    private suspend fun aggregateAverageQuantity(
-        calcQuantity: (List<NoteEntity>) -> Int?
-    ): Map<Weekday, Float> {
-        val noteEntityToDaysMap = noteEntityToDaysMap()
-        Timber.d("noteEntityToDaysMap=$noteEntityToDaysMap")
+    private fun fillDatabase() {
+        val exerciseEntity1 = ExerciseEntity(
+            id = 1.toId(),
+            name = EXERCISE_NAME_1
+        )
+        exerciseDao.insert(exerciseEntity1)
 
-        val weekdayToQuantityListMap = weekdayToEmptyIntListMap()
+        val exerciseEntity2 = ExerciseEntity(
+            id = 2.toId(),
+            name = EXERCISE_NAME_2
+        )
+        exerciseDao.insert(exerciseEntity2)
+
         val calendar = Calendar.getInstance()
-        noteEntityToDaysMap.keys.forEach { days ->
-            val weekday = days.date.toWeekday(calendar)
-            noteEntityToDaysMap[days]?.let { noteEntityList ->
-                calcQuantity(noteEntityList)?.let { quantity ->
-                    weekdayToQuantityListMap[weekday]?.add(quantity)
-                }
-            }
-        }
 
-        val averageQuantity = mutableMapOf<Weekday, Float>()
-        ALL_WEEKDAYS.forEach { weekday ->
-            val quantityList = weekdayToQuantityListMap[weekday]
-            if (quantityList.isNullOrEmpty().not()) {
-                quantityList?.average()?.let {
-                    averageQuantity[weekday] = it.toFloat()
-                }
-            }
-            Timber.d("weekday=$weekday, quantityList=$quantityList")
-            Timber.d("averageQuantity[$weekday]=${averageQuantity[weekday]}")
-        }
-        return averageQuantity
-    }
+        // Monday
+        calendar.set(Calendar.DAY_OF_WEEK, 2)
+        val noteEntity1 = NoteEntity(
+            id = 1.toId(),
+            timestamp = calendar.timeInMillis,
+            exerciseId = exerciseEntity1.id,
+            bpm = BPM[0]
+        )
+        noteDao.insert(noteEntity1)
 
-    private suspend fun noteEntityToDaysMap(): Map<Days, List<NoteEntity>> {
-        val noteEntities = noteDao.listAllUnorderedSuspending()
-        val noteEntityToDaysMap = mutableMapOf<Days, MutableList<NoteEntity>>()
-        noteEntities.forEach { note ->
-            val days = Days(note.timestamp)
-            val noteEntityList = noteEntityToDaysMap[days]
-                ?: mutableListOf<NoteEntity>()
-                    .also { noteEntityToDaysMap[days] = it }
-            noteEntityList.add(note)
-        }
-        return noteEntityToDaysMap
-    }
+        // Wednesday
+        calendar.set(Calendar.DAY_OF_WEEK, 4)
+        val noteEntity2 = NoteEntity(
+            id = 2.toId(),
+            timestamp = calendar.timeInMillis,
+            exerciseId = exerciseEntity2.id,
+            bpm = BPM[1]
+        )
+        noteDao.insert(noteEntity2)
 
-    private fun weekdayToEmptyIntListMap(): Map<Weekday, MutableList<Int>> {
-        val weekdayToIntListMap = mutableMapOf<Weekday, MutableList<Int>>()
-        ALL_WEEKDAYS.forEach { weekday ->
-            weekdayToIntListMap[weekday] = mutableListOf()
-        }
-        return weekdayToIntListMap
-    }
+        val noteEntity3 = NoteEntity(
+            id = 3.toId(),
+            timestamp = calendar.timeInMillis,
+            exerciseId = exerciseEntity2.id,
+            bpm = BPM[2]
+        )
+        noteDao.insert(noteEntity3)
 
-    private fun Date.toWeekday(calendar: Calendar): Weekday {
-        calendar.time = this
-        val weekdayNumber = calendar.get(Calendar.DAY_OF_WEEK)
-        return ALL_WEEKDAYS[weekdayNumber - 1]
+        calendar.set(Calendar.YEAR, 1992)
+        calendar.set(Calendar.DAY_OF_WEEK, 4)
+        val noteEntity4 = NoteEntity(
+            id = 4.toId(),
+            timestamp = calendar.timeInMillis,
+            exerciseId = exerciseEntity2.id,
+            bpm = BPM[3]
+        )
+        noteDao.insert(noteEntity4)
     }
 }
