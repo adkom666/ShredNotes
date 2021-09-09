1a1be532f96287d915eaac477564152fb523c2f7
adkom666 <adkom666@gmail.com>
2021-09-09 16:43:59 +0300

    Donation SKUs in one place

--- app/src/catappult/kotlin/com/adkom666/shrednotes/di/module/BillingModule.kt
+++ app/src/catappult/kotlin/com/adkom666/shrednotes/di/module/BillingModule.kt
@@ -1,5 +1,6 @@
 package com.adkom666.shrednotes.di.module
 
+import com.adkom666.shrednotes.BuildConfig
 import com.adkom666.shrednotes.ask.CatappultBillingFactory
 import com.adkom666.shrednotes.ask.template.GoogleLikeBillingFactory
 import dagger.Module
@@ -12,7 +13,7 @@ import javax.inject.Singleton
 class BillingModule {
 
     private companion object {
-        private const val SKU = "donate"
+        private const val SKU = BuildConfig.DONATION_SKU
     }
 
     @Provides
